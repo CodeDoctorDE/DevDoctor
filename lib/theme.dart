de5c9ffdfa8cc4f28d2674b00b1cc1e819c797d1
CodeDoctorDE <wusel905@gmail.com>
2020-07-07 10:51:40 +0200

    add fonts

---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+
+const mdcThemePrimary = Color(0xff3e82);
+const mdcThemeOnPrimary = Color(0xffffff);
+const mdcThemeSecondary = Color(0x3effba);
+const mdcThemeOnSecondary = Color(0x000000);
+const mdcThemeError = Color(0xb00020);
+const mdcThemeOnError = Color(0xffffff);
+const mdcThemeSurface = Color(0xffffff);
+const mdcThemeOnSurface = Color(0x000000);
+const mdcThemeBackground = Color(0xffffff);
+var mdcTypographyFontFamily = GoogleFonts.bellotaText();
